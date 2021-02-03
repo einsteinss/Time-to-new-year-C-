8bc4bda927a0bd7d39b4967bb0ee01fe1faec637
einsteinss <einsteinss23@gmail.com>
2021-02-03 16:13:51 +0000

    Terminei a interface do Usuario
    Todo Layout..

--- Time to new year C#/Time to new year C#/MainForm.Designer.cs
+++ Time to new year C#/Time to new year C#/MainForm.Designer.cs
@@ -21,6 +21,13 @@ namespace Time_to_new_year_C_
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Label label6;
 		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.PictureBox pictureBox1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -43,6 +50,7 @@ namespace Time_to_new_year_C_
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
@@ -50,12 +58,22 @@ namespace Time_to_new_year_C_
 			this.label5 = new System.Windows.Forms.Label();
 			this.label6 = new System.Windows.Forms.Label();
 			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
+			this.label1.BackColor = System.Drawing.Color.Transparent;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
-			this.label1.Location = new System.Drawing.Point(232, 245);
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(271, 269);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(94, 51);
 			this.label1.TabIndex = 0;
@@ -63,8 +81,10 @@ namespace Time_to_new_year_C_
 			// 
 			// label2
 			// 
+			this.label2.BackColor = System.Drawing.Color.Transparent;
 			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
-			this.label2.Location = new System.Drawing.Point(412, 245);
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(451, 269);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(70, 51);
 			this.label2.TabIndex = 1;
@@ -72,8 +92,10 @@ namespace Time_to_new_year_C_
 			// 
 			// label3
 			// 
+			this.label3.BackColor = System.Drawing.Color.Transparent;
 			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
-			this.label3.Location = new System.Drawing.Point(336, 245);
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(375, 269);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(70, 51);
 			this.label3.TabIndex = 2;
@@ -81,8 +103,10 @@ namespace Time_to_new_year_C_
 			// 
 			// label4
 			// 
+			this.label4.BackColor = System.Drawing.Color.Transparent;
 			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
-			this.label4.Location = new System.Drawing.Point(317, 245);
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.Location = new System.Drawing.Point(356, 269);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(24, 51);
 			this.label4.TabIndex = 3;
@@ -90,8 +114,10 @@ namespace Time_to_new_year_C_
 			// 
 			// label5
 			// 
+			this.label5.BackColor = System.Drawing.Color.Transparent;
 			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
-			this.label5.Location = new System.Drawing.Point(395, 245);
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(434, 269);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(24, 51);
 			this.label5.TabIndex = 4;
@@ -99,8 +125,10 @@ namespace Time_to_new_year_C_
 			// 
 			// label6
 			// 
+			this.label6.BackColor = System.Drawing.Color.Transparent;
 			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
-			this.label6.Location = new System.Drawing.Point(468, 245);
+			this.label6.ForeColor = System.Drawing.Color.White;
+			this.label6.Location = new System.Drawing.Point(507, 269);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(24, 51);
 			this.label6.TabIndex = 5;
@@ -108,19 +136,112 @@ namespace Time_to_new_year_C_
 			// 
 			// label7
 			// 
+			this.label7.BackColor = System.Drawing.Color.Transparent;
 			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
-			this.label7.Location = new System.Drawing.Point(488, 245);
+			this.label7.ForeColor = System.Drawing.Color.White;
+			this.label7.Location = new System.Drawing.Point(527, 269);
 			this.label7.Name = "label7";
 			this.label7.Size = new System.Drawing.Size(70, 51);
 			this.label7.TabIndex = 6;
 			this.label7.Text = "00";
 			// 
+			// label8
+			// 
+			this.label8.BackColor = System.Drawing.Color.Transparent;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+			this.label8.ForeColor = System.Drawing.Color.White;
+			this.label8.Location = new System.Drawing.Point(285, 320);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(61, 31);
+			this.label8.TabIndex = 7;
+			this.label8.Text = "Dias";
+			this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label9
+			// 
+			this.label9.BackColor = System.Drawing.Color.Transparent;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+			this.label9.ForeColor = System.Drawing.Color.White;
+			this.label9.Location = new System.Drawing.Point(379, 320);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(61, 31);
+			this.label9.TabIndex = 8;
+			this.label9.Text = "Dias";
+			this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label10
+			// 
+			this.label10.BackColor = System.Drawing.Color.Transparent;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+			this.label10.ForeColor = System.Drawing.Color.White;
+			this.label10.Location = new System.Drawing.Point(454, 320);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(61, 31);
+			this.label10.TabIndex = 9;
+			this.label10.Text = "Dias";
+			this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label11
+			// 
+			this.label11.BackColor = System.Drawing.Color.Transparent;
+			this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+			this.label11.ForeColor = System.Drawing.Color.White;
+			this.label11.Location = new System.Drawing.Point(531, 320);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(61, 31);
+			this.label11.TabIndex = 10;
+			this.label11.Text = "Dias";
+			this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label12
+			// 
+			this.label12.BackColor = System.Drawing.Color.Transparent;
+			this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F);
+			this.label12.ForeColor = System.Drawing.Color.White;
+			this.label12.Location = new System.Drawing.Point(304, 112);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(266, 53);
+			this.label12.TabIndex = 11;
+			this.label12.Text = "Novo Ano 2022";
+			this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label13
+			// 
+			this.label13.BackColor = System.Drawing.Color.Transparent;
+			this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+			this.label13.ForeColor = System.Drawing.Color.White;
+			this.label13.Location = new System.Drawing.Point(214, 195);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(429, 31);
+			this.label13.TabIndex = 12;
+			this.label13.Text = "Contagem regressiva para o novo Ano";
+			this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(802, 2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(66, 50);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 13;
+			this.pictureBox1.TabStop = false;
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
 			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.ClientSize = new System.Drawing.Size(818, 365);
+			this.ClientSize = new System.Drawing.Size(872, 376);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
 			this.Controls.Add(this.label7);
 			this.Controls.Add(this.label6);
 			this.Controls.Add(this.label5);
@@ -131,6 +252,7 @@ namespace Time_to_new_year_C_
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "MainForm";
 			this.Text = "Time to new year C#";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
